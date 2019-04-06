1fda48bfa0fe4113b94645500d13978ccfbf36da
liyujian <liyujian@baidu.com>
2019-04-07 00:37:48 +0800

    add geojson format

--- js/editor.js
+++ js/editor.js
@@ -8,20 +8,20 @@ var geoJsonInput;
 var downloadLink;
 
 function init() {
-    // Initialise the map.
-    map = new google.maps.Map(document.getElementById('map-holder'), {
-        center: {
-            lat: 53.5547142,
-            lng: 9.9689018
-        },
-        zoom: 16
-    });
-    map.data.setControls(['Point', 'LineString', 'Polygon']);
-    map.data.setStyle({
-        editable: true,
-        draggable: true
-    });
-    bindDataLayerListeners(map.data);
+    // 百度地图API功能
+    map = new BMap.Map("map-holder");    // 创建Map实例
+    map.centerAndZoom(new BMap.Point(116.404, 39.915), 11);  // 初始化地图,设置中心点坐标和地图级别
+    //添加地图类型控件
+    map.addControl(new BMap.MapTypeControl({
+        mapTypes:[
+            BMAP_NORMAL_MAP,
+            BMAP_HYBRID_MAP
+        ]}));     
+    map.setCurrentCity("北京");          // 设置地图显示的城市 此项是必须设置的
+    map.enableScrollWheelZoom(true);     //开启鼠标滚轮缩放
+
+
+    bindDataLayerListeners();
 
     // Retrieve HTML elements.
     dropContainer = document.getElementById('drop-container');
@@ -36,32 +36,67 @@ function init() {
     // Set up the drag and drop events.
     // First on common events.
     [mapContainer, dropContainer].forEach(function(container) {
-        google.maps.event.addDomListener(container, 'drop', handleDrop);
-        google.maps.event.addDomListener(container, 'dragover', showPanel);
+        addDomListener(container, 'drop', handleDrop);
+        addDomListener(container, 'dragover', showPanel);
     });
 
     // Then map-specific events.
-    google.maps.event.addDomListener(mapContainer, 'dragstart', showPanel);
-    google.maps.event.addDomListener(mapContainer, 'dragenter', showPanel);
+    addDomListener(mapContainer, 'dragstart', showPanel);
+    addDomListener(mapContainer, 'dragenter', showPanel);
 
     // Then the overlay specific events (since it only appears once drag starts).
-    google.maps.event.addDomListener(dropContainer, 'dragend', hidePanel);
-    google.maps.event.addDomListener(dropContainer, 'dragleave', hidePanel);
+    addDomListener(dropContainer, 'dragend', hidePanel);
+    addDomListener(dropContainer, 'dragleave', hidePanel);
     // Set up events for changing the geoJson input.
-    google.maps.event.addDomListener(
+    addDomListener(
         geoJsonInput,
         'input',
         refreshDataFromGeoJson);
-    google.maps.event.addDomListener(
+    addDomListener(
         geoJsonInput,
         'input',
         refreshDownloadLinkFromGeoJson);
 
     // Set up events for styling.
-    google.maps.event.addDomListener(window, 'resize', resizeGeoJsonInput);
-}
-google.maps.event.addDomListener(window, 'load', init);
+    addDomListener(window, 'resize', resizeGeoJsonInput);
 
+    addDrawingTool(map);
+}
+function addDomListener(element, type, callback){
+    element.addEventListener(type, callback)
+}
+addDomListener(window, 'load', init);
+
+function addDrawingTool(map){
+    var overlays = [];
+    var overlaycomplete = function(e){
+        overlays.push(e.overlay);
+    };
+    var styleOptions = {
+        strokeColor:"red",    //边线颜色。
+        fillColor:"red",      //填充颜色。当参数为空时，圆形将没有填充效果。
+        strokeWeight: 3,       //边线的宽度，以像素为单位。
+        strokeOpacity: 0.8,    //边线透明度，取值范围0 - 1。
+        fillOpacity: 0.6,      //填充的透明度，取值范围0 - 1。
+        strokeStyle: 'solid' //边线的样式，solid或dashed。
+    }
+    //实例化鼠标绘制工具
+    var drawingManager = new BMapLib.DrawingManager(map, {
+        isOpen: false, //是否开启绘制模式
+        enableDrawingTool: true, //是否显示工具栏
+        drawingToolOptions: {
+            anchor: BMAP_ANCHOR_TOP_RIGHT, //位置
+            offset: new BMap.Size(5, 5), //偏离值
+        },
+        circleOptions: styleOptions, //圆的样式
+        polylineOptions: styleOptions, //线的样式
+        polygonOptions: styleOptions, //多边形的样式
+        rectangleOptions: styleOptions //矩形的样式
+    });  
+     //添加鼠标绘制工具监听事件，用于获取绘制结果
+    drawingManager.addEventListener('overlaycomplete', overlaycomplete);
+
+}
 // Refresh different components from other components.
 function refreshGeoJsonFromData() {
     map.data.toGeoJson(function(geoJson) {
@@ -94,6 +129,10 @@ function refreshDataFromGeoJson() {
     map.data = newData;
     bindDataLayerListeners(newData);
     setGeoJsonValidity(true);
+
+
+
+    
 }
 
 // Refresh download link.
@@ -103,9 +142,9 @@ function refreshDownloadLinkFromGeoJson() {
 
 // Apply listeners to refresh the GeoJson display on a given data layer.
 function bindDataLayerListeners(dataLayer) {
-    dataLayer.addListener('addfeature', refreshGeoJsonFromData);
-    dataLayer.addListener('removefeature', refreshGeoJsonFromData);
-    dataLayer.addListener('setgeometry', refreshGeoJsonFromData);
+    // dataLayer.addListener('addfeature', refreshGeoJsonFromData);
+    // dataLayer.addListener('removefeature', refreshGeoJsonFromData);
+    // dataLayer.addListener('setgeometry', refreshGeoJsonFromData);
 }
 
 // Display the validity of geoJson.
